03281f50a64a083a52696c834017add42d6c3541
Stephan <schmid91@mail.com>
2024-07-17 17:24:19 -0700

    menu adpatation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ menu }) => {
-  const setGovlabLogo = useState<string | null>(null)[1];
+  const [govlabLogo, setGovlabLogo] = useState<string | null>(null);
 
   const govlabLogoUrl = 'https://content.smartercrowdsourcing.org/assets/8eaa4206-808d-48be-b7b0-be7912b8f74b';
   const burnesCenterLogoUrl = 'https://content.smartercrowdsourcing.org/assets/e8582384-957d-46d1-92e7-8d0dce0a27f2';
@@ -25,28 +25,36 @@ const Header: React.FC<HeaderProps> = ({ menu }) => {
     fetchGovLabLogo();
   }, []);
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+
   return (
     <header className={styles.header}>
-      <div className={styles.menuButton}>
-        <span className={styles.hamburger}>&#9776;</span>
-        <nav className={styles.footerNav}>
-            {menu.map((item, index) => (
-              <a key={index} href={item.link}>{item.label}</a>
-            ))}
-          </nav>
-      </div>
+    <div className={styles.menuButton} onClick={toggleMenu}>
+      <span className={styles.hamburger}>&#9776;</span>
+    </div>
+      <nav className={`${styles.headerNav} ${isMenuOpen ? styles.showMenu : ''}`}>
+    {menu.map((item, index) => (
+      <a key={index} href={item.link}>{item.label}</a>
+    ))}
+  </nav>
+      {
       <div className={styles.logos}>
-      <img 
+        <img 
           src={govlabLogoUrl} 
           alt="The Govlab" 
-          className={styles.burnesCenterLogo}
+          className={styles.govlabLogo}
         />
         <img 
           src={burnesCenterLogoUrl} 
           alt="Burnes Center" 
           className={styles.burnesCenterLogo}
         />
-      </div>
+      </div>}
     </header>
   );
 };
